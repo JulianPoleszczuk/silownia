e5e93dd868ffb26d623b00bfccd57ff254675417
Julian Poleszczuk <julianpoleszczuk37@gmail.com>
2026-04-29 11:29:27 +0200

    prosty panel admina

--- admin_panel.py
+++ admin_panel.py
@@ -1,6 +1,4 @@
-
 import customtkinter as ctk
-# from CustomTkinterMessagebox import *
 import json
 import os
 CZLONKOWIE = "czlonkowie.json"
@@ -8,34 +6,70 @@ def zapisywanie(nowy):
     if os.path.exists(CZLONKOWIE):
         with open(CZLONKOWIE, "r") as f:
             try:
-                lista_czlonkow = json.load(f)
-            except json.JSONDecodeError:
-                lista_czlonkow = []
+                lista = json.load(f)
+            except:
+                lista = []
     else:
-        lista_czlonkow = []
-    lista_czlonkow.append(nowy)
+        lista = []
+    lista.append(nowy)
     with open(CZLONKOWIE, "w") as f:
-        json.dump(lista_czlonkow, f, indent=4)
-def dodawanie_osob():
-    imie = entry_imie.get()
-    nazwisko = entry_nazwisko.get()
-    if imie and nazwisko:
-        dane_czlonka = {
-            "imie": imie,
-            "nazwisko": nazwisko,
-            "status": "Aktywny"
-        }
-        zapisywanie(dane_czlonka)
-        entry_imie.delete(0, ctk.END)
-        entry_nazwisko.delete(0, ctk.END)
+        json.dump(lista, f, indent=4)
+def okno_dodawania():
+    win = ctk.CTkToplevel(root)
+    win.title("Dodaj osobę")
+    win.geometry("300x200")
+
+    label1 = ctk.CTkLabel(win, text="Imię")
+    label1.pack(pady=5)
+    entry_imie = ctk.CTkEntry(win)
+    entry_imie.pack(pady=5)
+    label2 = ctk.CTkLabel(win, text="Nazwisko")
+    label2.pack(pady=5)
+    entry_nazwisko = ctk.CTkEntry(win)
+    entry_nazwisko.pack(pady=5)
+    def zapisz():
+        imie = entry_imie.get()
+        nazwisko = entry_nazwisko.get()
+
+        if imie and nazwisko:
+            dane = {
+                "imie": imie,
+                "nazwisko": nazwisko,
+                "status": "Aktywny"
+            }
+            zapisywanie(dane)
+            win.destroy()
+    btn = ctk.CTkButton(win, text="Zapisz", command=zapisz)
+    btn.pack(pady=10)
+def okno_usuwania():
+    win = ctk.CTkToplevel(root)
+    win.title("Usuń osobę")
+    win.geometry("300x200")
+
+    label1 = ctk.CTkLabel(win, text="Imię")
+    label1.pack(pady=5)
+
+    entry_imie = ctk.CTkEntry(win)
+    entry_imie.pack(pady=5)
+
+    label2 = ctk.CTkLabel(win, text="Nazwisko")
+    label2.pack(pady=5)
+
+    entry_nazwisko = ctk.CTkEntry(win)
+    entry_nazwisko.pack(pady=5)
+
+    btn = ctk.CTkButton(win, text="Usuń")
+    btn.pack(pady=10)
 root = ctk.CTk()
 root.title("Menadżer Członkostwa w Siłowni")
-root.geometry("400x400")
-entry_imie = ctk.CTkEntry(root,placeholder_text="imie")
-entry_imie.grid(row=0, column=1)
-entry_nazwisko = ctk.CTkEntry(root,placeholder_text="nazwisko")
-entry_nazwisko.grid(row=1, column=1)
-wyslij = (ctk.CTkButton(root, text="Dodaj", command=dodawanie_osob))
-wyslij.grid(row=2, column=1)
-root.mainloop()
+root.geometry("1024x768")
+tekst = ctk.CTkLabel(root, text="Panel Admina")
+tekst.pack(pady=30)
 
+przycisk_dodaj = ctk.CTkButton(root, text="Dodaj osobę", command=okno_dodawania)
+przycisk_dodaj.pack(pady=10)
+
+przycisk_usun = ctk.CTkButton(root, text="Usuń osobę", command=okno_usuwania)
+przycisk_usun.pack(pady=10)
+
+root.mainloop()
